8575e991b1e1a93068f20320f06d30363c8f751e
Siddarth <ss@jigsaw.vc>
2020-09-01 13:54:38 +0530

    refactor(tests): client test no longer uses cache

--- test/authlessClient.spec.ts
+++ test/authlessClient.spec.ts
@@ -1,20 +1,13 @@
 /* eslint-env node, jest */
-import { Client, ClientLowLevel } from '../src'
+import { Client } from '../src'
 
 describe('Client', () => {
   let authlessClient: Client | null = null
-  let authlessLowLevelClient: ClientLowLevel | null = null
-  beforeEach(() => {
-    authlessLowLevelClient = new ClientLowLevel({
-      retries: 0,
-      uri: 'http://example.com:4000',
-    })
-  })
 
   describe('new', () => {
     test('returns authlessClient instance', () => {
       authlessClient = new Client({
-        lowLevelClient: authlessLowLevelClient,
+        uri: 'http://example.com:4000',
         retries: 1,
       })
       expect(authlessClient).toBeInstanceOf(Client)
