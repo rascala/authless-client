d3db1ff62ebac1519e1f8001570fa012ef4b44a5
Siddarth <ss@jigsaw.vc>
2020-09-05 02:16:37 +0530

    refactor(test): check if Client throws on invalid config

--- test/authlessClient.spec.ts
+++ test/authlessClient.spec.ts
@@ -5,9 +5,17 @@ describe('Client', () => {
   let authlessClient: Client | null = null
 
   describe('new', () => {
+    test('rejects incorrect constructor config', () => {
+      const invalidConfig = {
+        serverUri: 'http://example.com:4000',
+        retries: 's' as unknown as number,
+      }
+      expect(() => new Client(invalidConfig)).toThrow()
+    })
+
     test('returns authlessClient instance', () => {
       authlessClient = new Client({
-        uri: 'http://example.com:4000',
+        serverUri: 'http://example.com:4000',
         retries: 1,
       })
       expect(authlessClient).toBeInstanceOf(Client)
